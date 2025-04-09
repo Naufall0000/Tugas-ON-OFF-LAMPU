ac0869aebde8ca7b9c3b4b249de60fef5dff790e
Naufall0000 <swastikanaufal@gmail.com>
2025-04-09 23:02:50 +0800

    menambahkan 3 lampu 1 saklar & 1 lampu 1 saklar & 2 lampu 1 saklar

--- saklar.js
+++ saklar.js
@@ -9,7 +9,14 @@ function saklar(action, lamp){
     let toggle8 = document.getElementById("default-toggle8");
     let toggle9 = document.getElementById("default-toggle9");
     let toggle10 = document.getElementById("default-toggle10");
-    
+    let toggle11 = document.getElementById("default-toggle11");
+    let toggle12 = document.getElementById("default-toggle12");
+    let toggle13 = document.getElementById("default-toggle13");
+    let toggle14 = document.getElementById("default-toggle14");
+    let toggle15 = document.getElementById("default-toggle15");
+    let toggle16 = document.getElementById("default-toggle16");
+
+
     let lampu1 = document.getElementById("lampu1");
     let lampu2 = document.getElementById("lampu2");
     let lampu3 = document.getElementById("lampu3");
@@ -20,6 +27,12 @@ function saklar(action, lamp){
     let lampu8 = document.getElementById("lampu8");
     let lampu9 = document.getElementById("lampu9");
     let lampu10 = document.getElementById("lampu10");
+    let lampu11 = document.getElementById("lampu11");
+    let lampu12 = document.getElementById("lampu12");
+    let lampu13 = document.getElementById("lampu13");
+    let lampu14 = document.getElementById("lampu14");
+    let lampu15 = document.getElementById("lampu15");
+    let lampu16 = document.getElementById("lampu16");
 
     if (toggle1.checked) {
         lampu1.src = "assets/images/on.gif" ;
@@ -71,4 +84,47 @@ function saklar(action, lamp){
     } else {
         lampu10.src = "assets/images/off.gif" ;
     }     
+
+
+    if (toggle12.checked) {
+        lampu11.src = "assets/images/on.gif" ;
+    } else {
+        lampu11.src = "assets/images/off.gif" ;
+    }    
+    if (toggle12.checked) {
+        lampu12.src = "assets/images/on.gif" ;
+    } else {
+        lampu12.src = "assets/images/off.gif" ;
+    }    
+    if (toggle12.checked) {
+        lampu13.src = "assets/images/on.gif" ;
+    } else {
+        lampu13.src = "assets/images/off.gif" ;
+    }    
+
+
+    if (toggle14.checked) {
+        lampu14.src = "assets/images/on.gif" ;
+    } else {
+        lampu14.src = "assets/images/off.gif" ;
+    } 
+
+
+
+
+
+
+    if (toggle15.checked) {
+        lampu15.src = "assets/images/on.gif" ;
+    } else {
+        lampu15.src = "assets/images/off.gif" ;
+    } 
+    if (toggle15.checked) {
+        lampu16.src = "assets/images/on.gif" ;
+    } else {
+        lampu16.src = "assets/images/off.gif" ;
+    } 
+     
+
+
 }
