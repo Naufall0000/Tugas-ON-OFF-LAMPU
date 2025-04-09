802215f5f3ef5d6e2cbc70f03c25475e70b5b7d4
Naufall0000 <swastikanaufal@gmail.com>
2025-04-09 20:41:03 +0800

    menambahkan let toggle dan let lampu dari 4 sampai 10 di saklar.js

--- saklar.js
+++ saklar.js
@@ -2,10 +2,24 @@ function saklar(action, lamp){
     let toggle1 = document.getElementById("default-toggle1");
     let toggle2 = document.getElementById("default-toggle2");
     let toggle3 = document.getElementById("default-toggle3");
+    let toggle4 = document.getElementById("default-toggle4");
+    let toggle5 = document.getElementById("default-toggle5");
+    let toggle6 = document.getElementById("default-toggle6");
+    let toggle7 = document.getElementById("default-toggle7");
+    let toggle8 = document.getElementById("default-toggle8");
+    let toggle9 = document.getElementById("default-toggle9");
+    let toggle10 = document.getElementById("default-toggle10");
     
     let lampu1 = document.getElementById("lampu1");
     let lampu2 = document.getElementById("lampu2");
     let lampu3 = document.getElementById("lampu3");
+    let lampu4 = document.getElementById("lampu4");
+    let lampu5 = document.getElementById("lampu5");
+    let lampu6 = document.getElementById("lampu6");
+    let lampu7 = document.getElementById("lampu7");
+    let lampu8 = document.getElementById("lampu8");
+    let lampu9 = document.getElementById("lampu9");
+    let lampu10 = document.getElementById("lampu10");
 
     if (toggle1.checked) {
         lampu1.src = "assets/images/on.gif" ;
